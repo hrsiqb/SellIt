031c940f7d4a9227af42410498c46e961650f8c7
haris <hrsiqb@gmail.com>
2020-10-09 18:29:03 +0500

    Updates

---
+++ precache-manifest.482c657c546e2b9791b7b625b3b57cbe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db9abc00aac216ea3f1c1759592172ae",
+    "url": "/SellIt/index.html"
+  },
+  {
+    "revision": "1aac5800b1de106eb05d",
+    "url": "/SellIt/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "3275fe9b53483b53d3f0",
+    "url": "/SellIt/static/css/main.f962325d.chunk.css"
+  },
+  {
+    "revision": "1aac5800b1de106eb05d",
+    "url": "/SellIt/static/js/2.0a61495a.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/SellIt/static/js/2.0a61495a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3275fe9b53483b53d3f0",
+    "url": "/SellIt/static/js/main.9f7bb622.chunk.js"
+  },
+  {
+    "revision": "1a3b702a6cf6185ee00d",
+    "url": "/SellIt/static/js/runtime-main.da3b8ac2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SellIt/static/media/logo.5d5d9eef.svg"
+  }
+]);
