d0794da5fd9fe9989c9dbd7f25e53de32b3bd724
haris <hrsiqb@gmail.com>
2020-10-22 00:07:39 +0500

    Updates

---
+++ precache-manifest.90b71446534ca1788de8b1066d6284ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c6448b63bb046f984f2912b8d20c3a7",
+    "url": "/SellIt/index.html"
+  },
+  {
+    "revision": "d1e1f16caebb0b26e9a4",
+    "url": "/SellIt/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "76c6b60762cf4f6285de",
+    "url": "/SellIt/static/css/main.1e2a2ad1.chunk.css"
+  },
+  {
+    "revision": "d1e1f16caebb0b26e9a4",
+    "url": "/SellIt/static/js/2.8bd1403f.chunk.js"
+  },
+  {
+    "revision": "9f8702cb55408414e24afc5ac73fd822",
+    "url": "/SellIt/static/js/2.8bd1403f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76c6b60762cf4f6285de",
+    "url": "/SellIt/static/js/main.760f1638.chunk.js"
+  },
+  {
+    "revision": "1a3b702a6cf6185ee00d",
+    "url": "/SellIt/static/js/runtime-main.da3b8ac2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SellIt/precache-manifest.ecbc23b410a5de84eac589b920c15486.js"
+  "/SellIt/precache-manifest.90b71446534ca1788de8b1066d6284ad.js"
 );
 
 self.addEventListener('message', (event) => {
